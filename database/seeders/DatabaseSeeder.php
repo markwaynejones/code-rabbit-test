c86a323ed0c05d58cbada2da82f2c02b3ca230a3
Mark Jones <mark.jones@digitalbalance.co.uk>
2024-12-11 21:54:38 +0000

    added objective functionality including endpoint for displaying all objectives

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Objective;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,5 +20,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Objective::factory()->count(10)->create();
     }
 }
